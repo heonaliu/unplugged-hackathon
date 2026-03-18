d2bf356fea7be31f144b2fa8570d5d03f5311eec
Heona <heonaliu@gmail.com>
2026-03-18 00:52:37 -0400

    fix error tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {
-      colors: {
-        beige: "#F6F5F2",
-        sage: "#A3B18A",
-        bluegray: "#8E9AAF",
-        charcoal: "#2F2F2F",
+    content: [],
+    theme: {
+      extend: {
+        colors: {
+          beige: "#F6F5F2",
+          sage: "#A3B18A",
+          bluegray: "#8E9AAF",
+          charcoal: "#2F2F2F",
+      },
     },
-  },
-  plugins: [],
+    plugins: [],
+  }
 }
 
