716fd71f41c9e6789e75736040f6e2d5f4992f04
Heona <heonaliu@gmail.com>
2026-03-18 01:27:15 -0400

    subheading adjust

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,7 +3,7 @@ export const Hero = () => {
     <section>
       <h1>Unplugged</h1>
       <p className="mt-6 text-lg md:text-xl text-bluegray max-w-xl">
-        A chill hackathon for everyone, regardless of experience. Join us for a weekend of
+        A chill hackathon for everyone, regardless of experience. Join us for a weekend of excitement!
       </p>
 
     </section>
