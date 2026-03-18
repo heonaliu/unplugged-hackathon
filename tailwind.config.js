99a94aba4b2add3f66a6a0572617fd62ac7fa11c
Heona <heonaliu@gmail.com>
2026-03-18 00:50:35 -0400

    tailwind configure global colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        beige: "#F6F5F2",
+        sage: "#A3B18A",
+        bluegray: "#8E9AAF",
+        charcoal: "#2F2F2F",
+    },
   },
   plugins: [],
 }
