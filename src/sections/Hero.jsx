99ec8ce82fb2e7328322ec8a45784c278e651b02
Heona <heonaliu@gmail.com>
2026-03-19 00:11:07 -0400

    add plug image with css (fixed the left1/2 alignment)

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,15 +1,26 @@
 import { Calendar, MapPin } from "lucide-react";
+import plugVector from "../assets/plug-vector.png";
 
 export const Hero = () => {
   return (
     <section className="min-h-screen flex flex-col items-center justify-center text-center px-6 pt-24">
-      <h1 className="text-6xl md:text-8xl font-extrabold">
-        Unplugged
-      </h1>
+      <img
+        src={plugVector}
+        alt="Plug Vector"
+        className="
+      absolute 
+      top-0 
+      -translate-x-1/2 
+      w-[100vw] 
+      max-w-none 
+      opacity-40 
+      pointer-events-none 
+      z-0
+    "
+      />
+      <h1 className="text-6xl md:text-8xl font-extrabold">Unplugged</h1>
 
-      <h2 className="text-4xl md:text-5xl mt-4 font-bold">
-        2026
-      </h2>
+      <h2 className="text-4xl md:text-5xl mt-4 font-bold">2026</h2>
 
       {/* INFO ROW */}
       <div className="flex flex-col md:flex-row items-center gap-6 mt-8 text-lg md:text-xl">
@@ -34,7 +45,6 @@ export const Hero = () => {
         A chill hackathon for everyone, regardless of experience. Join us for a
         weekend of excitement!
       </p>
-
     </section>
   );
 };
