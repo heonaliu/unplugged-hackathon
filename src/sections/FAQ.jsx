c441f2d9a56edd46a0d3b1d7ff28e9aba855ce34
Heona <heonaliu@gmail.com>
2026-03-20 00:05:33 -0400

    add additional faq questions

--- src/sections/FAQ.jsx
+++ src/sections/FAQ.jsx
@@ -6,14 +6,25 @@ export const Faq = () => {
         "The hackathon is open to all high school students, regardless of experience level. Whether you're a coding pro or just starting out, we welcome you to join us for a day of creativity and learning!",
     },
     {
+      question: "Do I need experience?",
+      answer:
+        "No! Before the hackathon, we will be hosting workshops to help beginners get started with programming.",
+    },
+    {
       question: "What should I bring to the hackathon?",
       answer:
-        "Bring your laptop, charger, and any materials you think will help you during the hackathon. We also recommend bringing a notebook and pen for jotting down ideas!",
+        "Bring your laptop, charger, and any materials you think will help you during the hackathon.",
     },
     {
       question: "Will there be food provided?",
       answer:
         "Yes! We will provide snacks, pizza, and drinks throughout the day.",
+    },
+
+    {
+      question: "Will there be prizes?",
+      answer:
+        "Yes! Prizes will be given to the top 3 projects rated based on creativity, originality, and technical complexity",
     },
   ];
 
@@ -30,7 +41,9 @@ export const Faq = () => {
           <h2 className="text-charcoal text-2xl font-semibold mb-4">
             {item.question}
           </h2>
-          <p className=" items-center ml-2 text-lg text-bluegray">{item.answer}</p>
+          <p className=" items-center ml-2 text-lg text-bluegray">
+            {item.answer}
+          </p>
         </div>
       ))}
     </section>
